b86c4ee91286cdca29de88796dc80a93f1cab670
Rodrigo <rodrigo.santoscg@gmail.com>
2019-11-24 15:59:43 -0300

    Commit inicial

---
+++ backend/migrations/20191023170949_create_table_articles.js
@@ -0,0 +1,25 @@
+const tableName = 'articles';
+
+exports.up = function(knex, Promise) {
+    return knex.schema.createTable(tableName, (table) => {
+        table.increments('id')
+            .primary()
+        table.string('name')
+            .notNull()
+        table.string('description', 1000)
+            .notNull()
+        table.string('imageUrl', 1000)
+        table.binary('content')
+            .notNull()
+        table.integer('userId')
+            .references('id')
+            .inTable('users')
+        table.integer('categoryId')
+            .references('id')
+            .inTable('categories')
+    })
+};
+
+exports.down = function(knex, Promise) {
+    return knex.schema.dropTable(tableName)
+};
