1bd6ceee56f26940bfbde706161fe415856d349c
Rodrigo <rodrigo.santoscg@gmail.com>
2019-11-24 16:10:28 -0300

    teste de cache de credencial do git

--- backend/api/article.js
+++ backend/api/article.js
@@ -1,10 +1,9 @@
 const queries = require('./queries')
 
-
 module.exports = (app) => {
 
     const { existsOrError, notExistsOrError } = app.api.validation
-    const limit = 10 
+    const limit = 10
 
     const save = (req, res) => {
 
@@ -46,7 +45,7 @@ module.exports = (app) => {
             } catch (err) {
                 return res.status(400).send(err)
             }
-            res.status(204).send() 
+            res.status(204).send()
 
         } catch (msg) {
             res.status(500).send(msg)
@@ -55,16 +54,16 @@ module.exports = (app) => {
 
     const get = async (req, res) => {
 
-        const page  = req.query.page || 1
+        const page = req.query.page || 1
         const result = await app.db('articles').count('id').first()
         const count = parseInt(result.count)
 
         app.db('articles')
             .select('id', 'name', 'description')
             .limit(limit).offset(page * limit - limit)
-            .then((articles) => res.json({ 
-                data: articles, 
-                count, 
+            .then((articles) => res.json({
+                data: articles,
+                count,
                 limit
             }))
             .catch((err) => res.status(500).send(err))
