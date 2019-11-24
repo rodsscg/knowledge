4f8bc1fe2eaedf059879c027dbaa396ef8f3a325
Rodrigo <rodrigo.santoscg@gmail.com>
2019-11-24 19:40:48 -0300

    Integração com mongodb para estatísticas; Integração entre mongo e postgres com schedule

--- backend/config/mongodb.js
+++ backend/config/mongodb.js
@@ -1,1 +1,7 @@
 const mongoose = require('mongoose')
+
+mongoose.connect('mongodb://mongo:mongo@localhost:27017/knowledge?authSource=admin', { useNewUrlParser: true })
+    .catch((e) => {
+        const msg = 'Erro! Não foi possível conectar com o MongoDB!'
+        console.log('\x1b[41m%s\x1b[37m', msg, '\x1b[0m', e)
+    })
