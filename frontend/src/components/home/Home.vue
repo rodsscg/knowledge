0b57a23e0b8d474d6d848b26723c222e617f63a0
Rodrigo <rodrigo.santoscg@gmail.com>
2019-12-08 12:40:25 -0300

    Dashboard e integração com backend

--- frontend/src/components/home/Home.vue
+++ frontend/src/components/home/Home.vue
@@ -1,18 +1,47 @@
 <template>
 <div class="home">
-  <PageTitle icon="fa fa-home" main="Dashboard" sub="Base de Conhecimento"/>
+  <PageTitle icon="fa fa-home" main="Dashboard" sub="Base de Conhecimento" />
+  <div class="stats">
+    <Stat color="#d54d50" title="Categorias" icon="fa fa-folder" :value="stat.categories" />
+    <Stat color="#3bc480" title="Artigos" icon="fa fa-file" :value="stat.articles" />
+    <Stat color="#3282cd" title="Usuários" icon="fa fa-user" :value="stat.users" />
+  </div>
 </div>
 </template>
 
 <script>
+import axios from 'axios';
+
 import PageTitle from '../templates/PageTitle.vue';
+import Stat from './Stat.vue';
+import baseApiUrl from '../../global';
 
 export default {
   name: 'Home',
-  components: { PageTitle },
+  components: { PageTitle, Stat },
+  methods: {
+    getStats() {
+      axios.get(`${baseApiUrl}/stats`)
+        .then(({ data }) => {
+          this.stat = data;
+        });
+    },
+  },
+  data() {
+    return {
+      stat: {},
+    };
+  },
+  mounted() {
+    this.getStats();
+  },
 };
 </script>
 
 <style>
-
+.stats {
+  display: flex;
+  justify-content: space-between;
+  flex-wrap: wrap;
+}
 </style>
--- frontend/src/main.js
+++ frontend/src/main.js
@@ -9,6 +9,9 @@ import router from './config/router';
 
 Vue.config.productionTip = false;
 
+// TODO: Temporário. Será adicionado a parte de autenticação posteriormente.
+require('axios').defaults.headers.common.Authorization = 'bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpZCI6MSwibmFtZSI6IlJvZHJpZ28iLCJlbWFpbCI6InJvZHJpZ29zYW50b3NjZ0BnbWFpbC5jb20iLCJhZG1pbiI6dHJ1ZSwiaWF0IjoxNTc1ODE4MTcyLCJleHAiOjE1NzYwNzczNzJ9.amcDL1F-pOr7EleuoUROJosKOKGYY5VAQF_TAoCweBs';
+
 new Vue({
   store,
   router,
